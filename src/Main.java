fa29fa1b093e30c70b588cba872a0aab956e0c36
MinhPham28 <142344042+minhphamMt@users.noreply.github.com>
2024-03-14 08:18:49 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,27 @@
+import java.io.*;
+
 public class Main {
     public static void main(String[] args) {
-
-
-        Bài làm viết tại đây
-
+        try {
+            FileReader in = new FileReader("input.txt");
+            BufferedReader br = new BufferedReader(in);
+            FileWriter out = new FileWriter("output.txt");
+            BufferedWriter bw = new BufferedWriter(out);
+            String line;
+            while ((line = br.readLine()) != null) {
+              
+                String modifiedLine = line.replaceAll("Nha Trang", "Vũng Tàu");
+                bw.write(modifiedLine);
+                bw.newLine(); 
+            }
+            bw.close();
+            out.close();
+            br.close();
+            in.close();
+            System.out.println("Thực hiện thay thế và ghi file output.txt thành công.");
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("Đã xảy ra lỗi khi thực hiện thay thế và ghi file output.txt.");
+        }
     }
 }
